89a95d61c0bcd88c59a36cbcfd23ea68a32d64af
Kishore N E <contactforkishore@gmail.com>
2025-04-27 23:35:51 +0530

    Time: 2 ms (2.99%), Space: 15.1 MB (66.69%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int size = nums.size();
+        if(size==1) {
+            return (nums[0]==target) ? 0 : -1;
+        }
+
+        bool sorted = true;
+        int start = nums[0];
+        int end = nums[size-1];
+
+        int left = 0;
+        int right = size-1;
+        int pivot = 0;
+
+        while(left<=right) {
+            int mid = left + (right - left) / 2;
+            if( nums[mid] > end ) {
+                if(nums[mid] > nums[mid+1]) {
+                    sorted = false;
+                    pivot = mid;
+                    break;
+                }
+                left = mid + 1;
+            }
+            else if( nums[mid] < end ) {
+                right = mid-1;
+            }
+        }
+
+        cout<<nums[pivot]<<endl;
+        if(sorted && pivot==0) {
+            left = 0;
+            right = size - 1;
+        }
+
+        else {
+            if(target >= start ) {
+                left = 0;
+                right = pivot;
+            }
+            else {
+                left = pivot+1;
+                right = size - 1;
+            }
+    
+            
+        }
+        cout<<left<<"->"<<right<<endl;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if( nums[mid] == target ) {
+                return mid;
+            }
+            else if( nums[mid] < target ) {
+                left = mid+1;
+            }
+            else {
+                right = mid-1;
+            }
+        }
+
+        return -1;
+    }
+};
