fdc9d4ea39405e909f23ea0424bb9149eb54e476
Kishore N E <contactforkishore@gmail.com>
2025-04-27 19:45:11 +0530

    Time: 0 ms (100%), Space: 32.1 MB (72.86%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -2,41 +2,23 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int size = nums.size();
-        int prefix[size+1];
 
-        prefix[0] = 0;
-        prefix[1] = nums[0];
-
-        for(int i=2 ; i<=size ; i++) {
-            prefix[i] = prefix[i-1] + nums[i-1];
-        }
-
-        for(int i=0 ; i<=size ; i++) {
-            cout<<prefix[i]<<" ";
-        }
-
-        int minWindow = 0;
-        int left = 1;
-        int right = 1;
+        int minWindow = size+1;
+        int left = 0;
         int tempSum = 0;
-        while( left<=size ) {
-            //cout<<"For index "<<left<<endl;
-            while( right<=size ) {
-                tempSum = (prefix[right] - prefix[left-1]);
-                //cout<<left<<"->"<<right<<" = "<<tempSum<<endl;
-                if(tempSum >= target) {
-                    if( minWindow == 0) {
-                        minWindow = right - left + 1;
-                        break;
-                    }
-                    minWindow = min(minWindow, (right-left+1));
-                    break;
-                }
-                right++;
-            }
-            left++;
-        }
+        
+        for(int right=0 ; right<size ; right++) {
+            tempSum += nums[right];
 
-        return minWindow;
+            while(tempSum >= target) {
+                minWindow = min(minWindow, right-left+1);
+                tempSum -= nums[left];
+                left++;
+            }
+
+
+        } 
+
+        return (minWindow==size+1) ? 0 : minWindow;
     }
 };
