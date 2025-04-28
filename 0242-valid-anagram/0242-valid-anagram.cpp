7d8522c980ea3ea9069743092ee5e6801cc26e34
Kishore N E <contactforkishore@gmail.com>
2025-04-28 23:50:51 +0530

    Time: 10 ms (8.64%), Space: 9.7 MB (48.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int size = s.size();
-        if(size != t.size())
-            return false;
+        // int size = s.size();
+        // if(size != t.size())
+        //     return false;
 
-        bool isAnagram = true;
-        map<char, int> map1;
-        for(int i=0 ; i<size ; i++) {
-            map1[s[i]]++;
-        }
+        // bool isAnagram = true;
+        // map<char, int> map1;
+        // for(int i=0 ; i<size ; i++) {
+        //     map1[s[i]]++;
+        // }
 
-        for(int i=0 ; i<size ; i++) {
-            if(map1.find(t[i]) == map1.end() || map1[t[i]] == 0) {
-                return false;
-            }
-            map1[t[i]]--;
-        }
+        // for(int i=0 ; i<size ; i++) {
+        //     if(map1.find(t[i]) == map1.end() || map1[t[i]] == 0) {
+        //         return false;
+        //     }
+        //     map1[t[i]]--;
+        // }
 
-        return isAnagram;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
+
+        //return isAnagram;
     }
 };
