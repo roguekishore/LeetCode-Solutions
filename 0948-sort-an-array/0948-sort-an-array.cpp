50b10fee4a49f67ab9f1f3818f681cc115981136
Kishore N E <contactforkishore@gmail.com>
2025-05-06 18:10:58 +0530

    Time: 55 ms (67.29%), Space: 73.2 MB (62.29%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    void merge(vector<int>& nums, int left, int mid, int right) {
-        int low = left; int size = right+1;
+    void merge(vector<int>& nums, int left, int mid, int right, vector<int>& temp) {
+        int low = left;
         int high = mid+1;
-        vector<int> temp;
+        int k = 0;
 
         while(low<=mid && high<=right) {
             if(nums[low]<=nums[high]) {
-                temp.push_back(nums[low++]);
+                temp[k++] = nums[low++];
             } else {
-                temp.push_back(nums[high++]);
+                temp[k++] = nums[high++];
             }
         }
 
         while(low<=mid)
-            temp.push_back(nums[low++]);
+            temp[k++] = nums[low++];
 
         while(high<=right)
-            temp.push_back(nums[high++]);
+            temp[k++] = nums[high++];
 
         for(int i=left ; i<=right ; i++) {
             nums[i] = temp[i-left];
         }
     }
-    void mergeSort(vector<int>& nums, int left, int right) {
+    void mergeSort(vector<int>& nums, int left, int right, vector<int>& temp) {
         if(left >= right) {
             return;
         }
         int mid = (left+right)/2;
-        mergeSort(nums, left, mid);
-        mergeSort(nums, mid+1, right);
-        merge(nums, left, mid, right);
+        mergeSort(nums, left, mid, temp);
+        mergeSort(nums, mid+1, right, temp);
+        merge(nums, left, mid, right, temp);
     }
     vector<int> sortArray(vector<int>& nums) {
-        // mergeSort(nums, 0, nums.size()-1);
-        sort(nums.begin(), nums.end());
+        vector<int> temp(nums.size());
+        mergeSort(nums, 0, nums.size()-1, temp);
         return nums;
     }
 };
