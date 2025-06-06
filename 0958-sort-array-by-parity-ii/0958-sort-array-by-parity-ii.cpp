37775dbf592c449062680d8d5988e514f66e6dbe
Kishore N E <contactforkishore@gmail.com>
2025-06-06 23:47:26 +0530

    Time: 0 ms (100%), Space: 25.9 MB (49.68%) - LeetHub

--- 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.cpp
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.cpp
@@ -4,7 +4,7 @@ public:
         int i = 0;
         int j = 1;
         int n = nums.size();
-        while(i<nums.size() && j<nums.size()) {
+        while(i<n && j<n) {
             while( i<n && nums[i]%2==0 ) {
                 i+=2;
             }
