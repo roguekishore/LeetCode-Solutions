f1ea58a9020d82de8d1a9651829ea2f7d6bef931
Kishore N E <contactforkishore@gmail.com>
2025-07-22 15:20:41 +0530

    Time: 4 ms (43.43%), Space: 14.2 MB (54.32%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int> temp;
+    void findCombinations(int start, int n, int target, vector<int>& candidates) {
+        if(target == 0) {
+            result.push_back(temp);
+            return;
+        }
+        if(target<0) {
+            return;
+        }
+        for(int i=start ; i<n ; i++) {
+            temp.push_back(candidates[i]);
+            findCombinations(i, n, target-candidates[i], candidates);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        findCombinations(0,candidates.size(),target,candidates);
+        return result;
+    }
+};
