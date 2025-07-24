a4dbee661c4dd47abe1d4577bb09877d9c10fa78
Kishore N E <contactforkishore@gmail.com>
2025-07-24 15:36:47 +0530

    Time: 2 ms (33.09%), Space: 14.4 MB (85.57%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     vector<int> grayCode(int n) {
         vector<int> result;
-        int total = pow(2, n);
-        result.push_back(0);
-        queue<int> q;
-        q.push(1);
-        bool left = true;
-        while(--total > 0) {
-            int num = q.front();
-            q.pop();
-            result.push_back(num);
-            if(!left) {
-                q.push(num<<1);
-                q.push(num<<1 | 1);
-            } else {
-                q.push(num<<1 | 1);
-                q.push(num<<1);
-            }
-            left = !left;
+        int total = 1<<n;
+        for(int i = 0 ; i<total ; i++) {
+            result.push_back(i ^ (i>>1));
         }
+        // result.push_back(0);
+        // queue<int> q;
+        // q.push(1);
+        // bool left = true;
+        // while(--total > 0) {
+        //     int num = q.front();
+        //     q.pop();
+        //     result.push_back(num);
+        //     if(!left) {
+        //         q.push(num<<1);
+        //         q.push(num<<1 | 1);
+        //     } else {
+        //         q.push(num<<1 | 1);
+        //         q.push(num<<1);
+        //     }
+        //     left = !left;
+        // }
         return result;
     }
 };
