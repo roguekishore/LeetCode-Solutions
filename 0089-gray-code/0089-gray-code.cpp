e819bbc1f22580acfe19623b840f8a610c6389dd
Kishore N E <contactforkishore@gmail.com>
2025-07-24 15:37:04 +0530

    Time: 3 ms (26.13%), Space: 14.7 MB (32.3%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -6,23 +6,6 @@ public:
         for(int i = 0 ; i<total ; i++) {
             result.push_back(i ^ (i>>1));
         }
-        // result.push_back(0);
-        // queue<int> q;
-        // q.push(1);
-        // bool left = true;
-        // while(--total > 0) {
-        //     int num = q.front();
-        //     q.pop();
-        //     result.push_back(num);
-        //     if(!left) {
-        //         q.push(num<<1);
-        //         q.push(num<<1 | 1);
-        //     } else {
-        //         q.push(num<<1 | 1);
-        //         q.push(num<<1);
-        //     }
-        //     left = !left;
-        // }
         return result;
     }
 };
