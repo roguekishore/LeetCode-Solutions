2e1d4d57fcd1f248475587f00195fc4da6cb517c
Kishore N E <contactforkishore@gmail.com>
2025-07-27 19:17:34 +0530

    Time: 11 ms (29.6%), Space: 13.2 MB (46.09%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -21,13 +21,6 @@ public:
                 }
             }
         }
-
-        for(int i=0 ; i<=n ; i++) {
-            for(int j=0 ; j<=m ; j++) {
-                cout<<dp[i][j]<<" ";
-            }
-            cout<<endl;
-        }
         return dp[n][m];
     }
 };
