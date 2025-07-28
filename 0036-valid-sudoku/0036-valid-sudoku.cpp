003331a72026721b7f16f603306d9091a6891b91
Kishore N E <contactforkishore@gmail.com>
2025-07-28 17:38:31 +0530

    Time: 4 ms (52.85%), Space: 22.8 MB (67.94%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,51 +1,25 @@
 class Solution {
 public:
-    bool solve(int x, int y, vector<vector<char>>& board) {
-        if(y==9)
-            return solve(x+1,0,board);
-        if(x==9)
-            return true;
-        if(board[x][y] == '.')
-            return solve(x,y+1,board);
+    vector<vector<bool>> row{9, vector<bool>(10, false)};
+    vector<vector<bool>> col{9, vector<bool>(10, false)};
+    vector<vector<bool>> box{9, vector<bool>(10, false)};
 
-        vector<bool> row(9, false);
-        vector<bool> col(9, false);
-        vector<bool> box(9, false);
-
+    bool isValidSudoku(vector<vector<char>>& board) {
         for(int i=0 ; i<9 ; i++) {
-            if(board[x][i] == '.') continue;
-            int num = board[x][i] - '0';
-            if(row[num])
-                return false;
-            row[num] = true;
-        }
+            for(int j=0 ; j<9 ; j++) {
+                if(board[i][j]=='.')
+                    continue;
 
-        for(int i=0 ; i<9 ; i++) {
-            if(board[i][y] == '.') continue;
-            int num = board[i][y] - '0';
-            if(col[num])
-                return false;
-            col[num] = true;
-        }
-
-        int dr = (x/3)*3;
-        int dc = (y/3)*3;
-        for(int i=dr ; i<dr+3 ; i++) {
-            for(int j=dc ; j<dc+3 ; j++) {
-                if(board[i][j] == '.') continue;
                 int num = board[i][j] - '0';
-                if(box[num])
+
+                int boxIndex = (i/3)*3 + j/3;
+                if(row[i][num] || col[j][num] || box[boxIndex][num]) {
                     return false;
-                box[num] = true;
+                }
+                row[i][num] = col[j][num] = box[boxIndex][num] = true;
             }
         }
-        
-        if(solve(x,y+1,board))
-            return true;
+        return true;
+    }
 
-        return false;
-    }
-    bool isValidSudoku(vector<vector<char>>& board) {
-        return solve(0,0,board);
-    }
 };
