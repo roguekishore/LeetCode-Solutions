0bbb5cb47e73cce637154ab1ef0052c11a35de41
Kishore N E <contactforkishore@gmail.com>
2025-07-30 10:23:42 +0530

    Time: 1057 ms (5%), Space: 31 MB (58.32%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<vector<int>> dp(row, vector<int>(col, 0));
+
+        int maxA = 0;
+        for(int i=0 ; i<row ; i++) {
+            dp[i][0] = matrix[i][0] - '0';
+            maxA = max(maxA, dp[i][0]);
+        }
+
+        for(int i=0 ; i<col ; i++) {
+            dp[0][i] = matrix[0][i] - '0';
+            maxA = max(maxA, dp[0][i]);
+        }
+        for(int i=1 ; i<row ; i++) {
+            for(int j=1 ; j<col ; j++) {
+                if(matrix[i][j] == '1') {
+                    dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + 1;
+                    maxA = max(maxA, dp[i][j]);
+                }
+            }
+        }
+        for(int i=0 ; i<row ; i++) {
+            for(int j=0 ; j<col ; j++) {
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        return maxA*maxA;
+    }
+};
